13df37776e0d4c372399c957b53724c1a34c06f7
anhhuy2110 <bahuy21102001@gmail.com>
2023-06-26 12:06:38 +0700

    no message

--- android/app/src/main/kotlin/vn/dotb/test/untitled1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package vn.dotb.test.interview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-part of './app_pages.dart';
-
-abstract class Routes {
-  static const INITIAL = '/';
-  static const HOME = '/Home';
-  static const HOMEMAIN = '/homeMain';
-  static const CREATETASK = '/CreateTask.dart';
-}
